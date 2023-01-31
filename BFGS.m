4d116d1ee0f1d2c6ddaad2628d70d13983908a40
LZiwenShen <124037487+LZiwenShen@users.noreply.github.com>
2023-01-31 17:33:20 +0800

    Add files via upload

---
+++ BFGS.m
@@ -0,0 +1,55 @@
+m = 5;
+n = 3;
+y_0 = 1;
+w = sym('w',[1 n]);
+v = sym('v',[1 n]);
+theta = sym('theta',[1 n]);
+s1 = 0;
+s2 = 0;
+y = 0;
+for i = 1:m
+    for j = 1:n
+        s1 = s1 + v(j) * 1 / (1 + exp(theta(j) - w(j) * i / m));
+        s2 = s1+v(j)*w(j)*exp(theta(j)-w(j)*i/m)/(1+exp(theta(j)-w(j)*i/m))^2;
+    end
+    y = y+((y_0+i/m*s1-(i/m)^2)*(i/m+(1+3*(i/m)^2)/(1+i/m+(i/m)^3))-2*i/m+s1+i/m*s2)^2;
+end
+f(w,v,theta) = y;
+p = [w,v,theta];
+g = sym('g',[3*n 1]);
+for i = 1:3*n
+    g(i) = diff(f,p(i));
+end
+p1 = ones(3*n,1);
+H = eye(3*n);
+epsilon = 1.5;
+k = 0;
+while(f(p1(1),p1(2),p1(3),p1(4),p1(5),p1(6),p1(7),p1(8),p1(9))>epsilon && k<1000)
+    g1 = zeros(3*n,1);
+    for i = 1:3*n
+        G(p) = g(i);
+        g1(i) = vpa(G(p1(1),p1(2),p1(3),p1(4),p1(5),p1(6),p1(7),p1(8),p1(9)));
+    end
+    d = - H * g1;
+    p2 = p1 + 0.0001 * d;
+    g2 = zeros(3*n,1);
+    for i = 1:3*n
+        G(p) = g(i);
+        g2 = vpa(G(p2(1),p2(2),p2(3),p2(4),p2(5),p2(6),p1(7),p1(8),p1(9)));
+    end
+    s = d;
+    y = g2 - g1;
+    T1 = H * y;
+    T2 = y.' * T1;
+    T3 = y.' * s;
+    T4 = T2 / T3;
+    T5 = s * s.';
+    T6 = T5 / T3;
+    D1 = (1 + T4) * T6;
+    T7 = s * T1.';
+    D2 = (T7 + T7.') / T3;
+    H = H + D1 - D2;
+    p1 = p2
+    k = k + 1
+    vpa(f(p1(1),p1(2),p1(3),p1(4),p1(5),p1(6),p1(7),p1(8),p1(9)))
+end
---
+++ run.m
@@ -0,0 +1,61 @@
+m = 5;
+n = 2;
+y_0 = 1;
+w = sym('w',[1 n]);
+v = sym('v',[1 n]);
+theta = sym('theta',[1 n]);
+s1 = 0;
+s2 = 0;
+y = 0;
+for i = 1:m
+    for j = 1:n
+        s1 = s1 + v(j) * 1 / (1 + exp(theta(j) - w(j) * i / m));
+        s2 = s1+v(j)*w(j)*exp(theta(j)-w(j)*i/m)/(1+exp(theta(j)-w(j)*i/m))^2;
+    end
+    y = y+((y_0+i/m*s1-(i/m)^2)*(i/m+(1+3*(i/m)^2)/(1+i/m+(i/m)^3))-2*i/m+s1+i/m*s2)^2;
+end
+f(w,v,theta) = y;
+p = [w,v,theta];
+g = sym('g',[3*n 1]);
+for i = 1:3*n
+    g(i) = diff(f,p(i));
+end
+p1 = ones(3*n,1);
+H = eye(3*n);
+epsilon = 1.5;
+while(f(p1(1),p1(2),p1(3),p1(4),p1(5),p1(6))>epsilon && k<1000)
+    g1 = zeros(3*n,1);
+    for i = 1:3*n
+        G(p) = g(i);
+        g1(i) = vpa(G(p1(1),p1(2),p1(3),p1(4),p1(5),p1(6)));
+    end
+    d = - H * g1;
+    p2 = p1 + 0.0001 * d;
+    g2 = zeros(3*n,1);
+    for i = 1:3*n
+        G(p) = g(i);
+        g2 = vpa(G(p2(1),p2(2),p2(3),p2(4),p2(5),p2(6)));
+    end
+    s = d;
+    y = g2 - g1;
+    T1 = H * y;
+    T2 = y.' * T1;
+    T3 = y.' * s;
+    T4 = T2 / T3;
+    T5 = s * s.';
+    T6 = T5 / T3;
+    D1 = (1 + T4) * T6;
+    T7 = s * T1.';
+    D2 = (T7 + T7.') / T3;
+    H = H + D1 - D2;
+    p1 = p2;
+end
+syms x;
+s3 = 0;
+for i = 1:n
+    s3 = s3 + p1(n + i) * 1 / (1 + exp(p1(2 * n + i) - p1(i) * x));
+end
+y_t(x) = y_0 + x * s3;
+y_1(x) = exp(- x ^ 2 / 2) / (1 + x + x ^ 3) + x ^ 2;
+t = 0:0.01:1;
+plot(t,y_t(t),'r',t,y_1(t),'b');
